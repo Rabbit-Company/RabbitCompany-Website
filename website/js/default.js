dc216935f7a3428f8e4e7eac0796c76926181fdf
Ziga Zajc <ziga.zajc007@gmail.com>
2024-06-01 19:52:13 +0200

    Update

--- website/js/default.js
+++ website/js/default.js
@@ -1,26 +1,29 @@
 document.getElementById("main-menu-btn").addEventListener("click", () => {
-    if(document.getElementById("mobile-menu").className == "sm:hidden hidden"){
-        document.getElementById("mobile-menu").className = "sm:hidden";
-        document.getElementById("main-menu-icon").innerHTML = "<path stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='M6 18L18 6M6 6l12 12' />";
-        return;
-    }
-    document.getElementById("mobile-menu").className = "sm:hidden hidden";
-    document.getElementById("main-menu-icon").innerHTML = "<path stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='M4 6h16M4 12h16M4 18h16' />";
+	if(document.getElementById("mobile-menu").className == "sm:hidden hidden"){
+		document.getElementById("mobile-menu").className = "sm:hidden";
+		document.getElementById("main-menu-icon").innerHTML = "<path stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='M6 18L18 6M6 6l12 12' />";
+		return;
+	}
+	document.getElementById("mobile-menu").className = "sm:hidden hidden";
+	document.getElementById("main-menu-icon").innerHTML = "<path stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='M4 6h16M4 12h16M4 18h16' />";
 });
 
+const footerYear = document.getElementById("footer-year");
+if(footerYear) footerYear.innerHTML = new Date().getFullYear() + 1;
+
 function copyToClipboard(text){
-    let textArea = document.createElement("textarea");
-    textArea.value = text;
+	let textArea = document.createElement("textarea");
+	textArea.value = text;
 
-    textArea.style.top = 0;
-    textArea.style.left = 0;
-    textArea.style.position = "fixed";
+	textArea.style.top = 0;
+	textArea.style.left = 0;
+	textArea.style.position = "fixed";
 
-    document.body.appendChild(textArea);
-    textArea.focus();
-    textArea.select();
+	document.body.appendChild(textArea);
+	textArea.focus();
+	textArea.select();
 
-    document.execCommand('copy');
+	document.execCommand('copy');
 
-    document.body.removeChild(textArea);
+	document.body.removeChild(textArea);
 }
