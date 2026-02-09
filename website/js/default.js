37e98579ae33a0979337d5abc201042b2ecf4794
Ziga Zajc <ziga@zajc.email>
2026-02-09 13:55:39 +0100

    Update fix copyright year

--- website/js/default.js
+++ website/js/default.js
@@ -1,17 +1,19 @@
 document.getElementById("main-menu-btn").addEventListener("click", () => {
-	if(document.getElementById("mobile-menu").className == "sm:hidden hidden"){
+	if (document.getElementById("mobile-menu").className == "sm:hidden hidden") {
 		document.getElementById("mobile-menu").className = "sm:hidden";
-		document.getElementById("main-menu-icon").innerHTML = "<path stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='M6 18L18 6M6 6l12 12' />";
+		document.getElementById("main-menu-icon").innerHTML =
+			"<path stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='M6 18L18 6M6 6l12 12' />";
 		return;
 	}
 	document.getElementById("mobile-menu").className = "sm:hidden hidden";
-	document.getElementById("main-menu-icon").innerHTML = "<path stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='M4 6h16M4 12h16M4 18h16' />";
+	document.getElementById("main-menu-icon").innerHTML =
+		"<path stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='M4 6h16M4 12h16M4 18h16' />";
 });
 
 const footerYear = document.getElementById("footer-year");
-if(footerYear) footerYear.innerHTML = new Date().getFullYear() + 1;
+if (footerYear) footerYear.innerHTML = new Date().getFullYear();
 
-function copyToClipboard(text){
+function copyToClipboard(text) {
 	let textArea = document.createElement("textarea");
 	textArea.value = text;
 
@@ -23,7 +25,7 @@ function copyToClipboard(text){
 	textArea.focus();
 	textArea.select();
 
-	document.execCommand('copy');
+	document.execCommand("copy");
 
 	document.body.removeChild(textArea);
 }
